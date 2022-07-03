b2826265229ffc96141e5464a04bd9d48f5e14de
Danila Kalay <ChechikLud@gmail.com>
2022-07-03 22:04:03 +0300

    finish commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,11 +6,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StatisticsServiceTest {
 
+
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
@@ -18,14 +19,15 @@ public class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
     @Test
-    void findMin() {
+    void findMaxNew() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 3;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 17, 8, 6, 11, 11, 12};
+        long expected = 17;
 
-        long actual = service.findMin(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+
 }
